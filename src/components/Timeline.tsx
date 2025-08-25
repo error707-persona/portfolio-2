2c805dcdfea45fecac243c849556d1789b3acb20
error707-persona <areeshasayed09@gmail.com>
2025-08-26 00:26:28 +0530

    added skills tag divs

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -38,29 +38,29 @@ const TimelineCards = () => {
 <div>
 
 
-          <div className="p-2 px-4 bg-purple-700 text-white rounded-full mr-5">
+          <div className="p-2 max-w-fit px-4 bg-purple-700 text-white rounded-full mr-5">
             ReactJS
           </div>
-          <div className="p-2 px-4 bg-purple-700 text-white rounded-full mr-5">
+          <div className="p-2 max-w-fit px-4 bg-purple-700 text-white rounded-full mr-5">
             Spring Boot
           </div>
-          <div className="p-2 px-4 bg-purple-700 text-white rounded-full mr-5">
+          <div className="p-2 max-w-fit px-4 bg-purple-700 text-white rounded-full mr-5">
             SQL
           </div>
 
-          <div className="p-2 px-4 bg-purple-700 text-white rounded-full mr-5">
+          <div className="p-2 max-w-fit px-4 bg-purple-700 text-white rounded-full mr-5">
             PL/SQL
           </div>
-          <div className="p-2 px-4 bg-purple-700 text-white rounded-full mr-5">
+          <div className="p-2 max-w-fit px-4 bg-purple-700 text-white rounded-full mr-5">
             Kafka
           </div>
-          <div className="p-2 px-4 bg-purple-700 text-white rounded-full mr-5 mt-3">
+          <div className="p-2 max-w-fit px-4 bg-purple-700 text-white rounded-full mr-5 mt-3">
             JavaScript
           </div>
-          <div className="p-2 px-4 bg-purple-700 text-white rounded-full mr-5 mt-3">
+          <div className="p-2 max-w-fit px-4 bg-purple-700 text-white rounded-full mr-5 mt-3">
             HTML/CSS
           </div>
-          <div className="p-2 px-4 bg-purple-700 text-white rounded-full mt-3">
+          <div className="p-2 max-w-fit px-4 bg-purple-700 text-white rounded-full mt-3">
             Kubernetes
           </div>
           </div>
